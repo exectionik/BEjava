0663d38e3809721a9396ccf389a61ecd8713701a
exectionik <exectionik9@gmail.com>
2020-11-14 20:41:58 +0100

    opravene dufam xd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,13 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-
+import java.util.stream.Collectors;
 
 
 public class Main {
+
+
+
 
     public static void main(String[] args) {
 
@@ -14,9 +16,13 @@ public class Main {
         pepega.add(1);
         pepega.add(12);
         pepega.add(142);
+        pepega.add(7);
 
-        pepega.removeIf(n -> n%2==0);
-        System.out.println(pepega);
+        List<Integer> neparneCisla = pepega
+            .stream()
+            .filter(n -> n % 2 !=0)
+            .collect(Collectors.toList());
+        System.out.println(neparneCisla);
 
 
 
